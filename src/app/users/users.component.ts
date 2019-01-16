18a986c89ac4631a96dbe0a8a04d412e8873558f
Yisal Khan <yisal.khan@square63.com>
2019-01-16 18:35:50 +0500

    Display users information on Home Page

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,39 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Users } from '../shared/users.module';
+import { UsersService } from '../shared/users.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, OnDestroy {
 
+  users : Users[];
+  subscription : Subscription;
   constructor(
     private router : Router,
     private route: ActivatedRoute,
+    private usersService : UsersService,
   ) { }
 
-  users = [
-    {
-      id : '001',
-      name : 'John',
-      gender : 'Male',
-      occupation : 'Electrician',
-      employee : 'Government'
-    },
-    {
-      id : '002',
-      name : 'George',
-      gender : 'Male',
-      occupation : 'Software Engineer',
-      employee : 'Private'
-    },
-  ];
+  ngOnInit() {
+    this.subscription = this.usersService.userChanged.subscribe(
+      (users : Users[]) => {
+        this.users = users;
+        console.log(users);
+      }
+    );
+    this.users = this.usersService.getUsers();
+  }
 
   onNewUser() {
     this.router.navigate(['new'], {relativeTo: this.route});
   }
-  ngOnInit() {
+
+  onEditUser(index:number) {
+    this.router.navigate([index+'/edit'], {relativeTo: this.route});
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
